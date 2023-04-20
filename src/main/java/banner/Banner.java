6ab290db4b15d6da327d4f189f7f98b13757ac60
davinsrma <106535705+davinsrma@users.noreply.github.com>
2023-04-20 21:04:48 +0530

    Banner Class Updated

--- src/main/java/banner/Banner.java
+++ src/main/java/banner/Banner.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
 public class Banner {
 
     public void bannerReader() throws IOException {
